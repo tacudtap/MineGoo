c0ebfe3f56b333335a4b573bcad3170ac339b723
Pat <tacudtap@gmail.com>
2020-03-11 04:14:30 -0400

    updated .gitignore

--- src/mineGoo/BuildTools.java
+++ src/mineGoo/BuildTools.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 
 public class BuildTools {
 
-    private static Path localPath = Paths.get(System.getProperty("user.home"), "sever", "buildtools");
+    private static Path localPath = Paths.get(System.getProperty("user.home"), "server", "buildtools");
 
     public static void buildDirectory() {
         try {
